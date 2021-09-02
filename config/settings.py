dcd12610658321d842118b72c82346bcc862bcde
kangraemin <remin1994@gmail.com>
2021-09-03 02:01:07 +0900

    [Add] Dotenv import in management.py

--- config/settings.py
+++ config/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-a7vh%+q+t*bxlttxn%++g(%+t$&h6(reuo+^wvmg3w=3fgwi9'
+# SECURITY WARNING: keep the secret key used in production secret!print(os.environ.get("DJANGO_SECRET_KEY"))
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
